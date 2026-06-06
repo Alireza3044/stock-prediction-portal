0211c186791bea4ec0d159eb8e6bf770f5f9f7e8
Alireza Mohammadi <alireza1384928@gmail.com>
2026-06-06 20:57:35 +0330

    Add temporary sample view.

--- backend-drf/api_app/views.py
+++ backend-drf/api_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+
+class SampleView(RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def retrieve(self, request):
+        return Response("Fetched the sample auth-required view.")
