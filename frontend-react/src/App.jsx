890a5d47fe94493e0c17b7868e230deef233633d
Alireza Mohammadi <alireza1384928@gmail.com>
2026-06-21 17:31:00 +0330

    Permitting access if token is expired

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,29 +1,43 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route, useNavigate } from "react-router-dom"
 import "./assets/css/app.css"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Register from "./components/Register"
 import Login from "./components/Login"
-import AuthProvider from "./Contexts"
+import { AuthContext } from "./Contexts"
 import Dashboard from "./components/Dashboard"
 import PrivateRoute from "./PrivateRoute"
 import PublicRoute from "./PublicRoute"
+import { useContext, useEffect } from "react"
 
 function App() {
+  const { setIsLoggedIn } = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const handleAuthRedirect = () => {
+      setIsLoggedIn(false)
+      navigate("/login")
+    }
+    window.addEventListener("refresh-token-expired", handleAuthRedirect)
+
+    return () => {
+      window.removeEventListener("refresh-token-expired", handleAuthRedirect)
+    }
+  }, [navigate, setIsLoggedIn])
+
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="register" element={<PublicRoute><Register /></PublicRoute>} />
-          <Route path="login" element={<PublicRoute><Login /></PublicRoute>} />
-          <Route path="dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </AuthProvider>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="register" element={<PublicRoute><Register /></PublicRoute>} />
+        <Route path="login" element={<PublicRoute><Login /></PublicRoute>} />
+        <Route path="dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
 
