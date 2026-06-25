f84f5956e68f6d87cc07d1c311aea33b93ac28a1
Alireza Mohammadi <alireza1384928@gmail.com>
2026-06-25 16:42:49 +0330

    Send plot image paths and evaluations to frontend

--- backend-drf/api_app/prediction.py
+++ backend-drf/api_app/prediction.py
@@ -1,9 +1,15 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 import yfinance as yf
 from datetime import datetime
+from django.conf import settings
+from keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, r2_score
+from math import floor
 
-def get_data(ticker: str) -> pd.DataFrame:
+def get_stock_price_data(ticker: str) -> pd.DataFrame:
     now = datetime.now()
     start = datetime(now.year - 10, now.month, now.day)
 
@@ -13,6 +19,69 @@ def get_data(ticker: str) -> pd.DataFrame:
 
     df.columns = df.columns.droplevel(1)
     df.columns.names = ['']
-    df = df.reset_index()
+    df.reset_index(inplace=True)
+    
+    data = pd.DataFrame(df.Close)
+    data["ma100"] = data.Close.rolling(100).mean()
+    data["ma200"] = data.Close.rolling(200).mean()
+    
+    return data
 
-    return df
+
+def plot_and_get_image_path(ticker, data, colors, plot_labels):
+    plt.switch_backend('AGG')
+    plt.figure(figsize=(12, 6))
+    for d, color, label in zip(data, colors, plot_labels):
+        print(d, color, label)
+        plt.plot(d, color, label=label)
+    plt.xlabel("Day")
+    plt.ylabel("Price")
+    plt.title(f"Ticker: {ticker}")
+    plt.legend()
+
+    plot_labels_text = "_".join(plot_labels)
+    plot_image_name = f"{ticker}_{plot_labels_text}_plot.png".replace(" ", "_")
+    plot_image_path = settings.MEDIA_ROOT / plot_image_name
+    plt.savefig(plot_image_path)
+    plt.close()
+
+    return settings.MEDIA_URL + plot_image_name
+
+
+def evaluate(data, model_path):
+    # Data preprocessing
+    data = pd.DataFrame(data)
+    index = floor(len(data) * 0.7)
+
+    test_data = data.iloc[index-100:]
+    
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_test_data = scaler.fit_transform(test_data)
+
+    x_test, y_test = [], []
+    for i in range(100, scaled_test_data.shape[0]):
+        x_test.append(scaled_test_data[i-100 : i])
+        y_test.append(scaled_test_data[i, 0])
+
+    x_test, y_test = np.array(x_test), np.array(y_test)
+    
+    # Loading and Prediction
+    model = load_model(model_path)
+    y_pred = model.predict(x_test)
+
+    # Get original data
+    y_test = scaler.inverse_transform(y_test.reshape(-1, 1))
+    y_pred = scaler.inverse_transform(y_pred.reshape(-1, 1))
+
+    # Evaluation
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = mse ** 0.5
+    r2 = r2_score(y_test, y_pred)
+    
+    return {
+        "y_test": y_test,
+        "y_pred": y_pred,
+        "mse": f"{mse:.3f}",
+        "rmse": f"{rmse:.3f}",
+        "r2_score": f"{r2:.3f}"
+    }
--- backend-drf/api_app/views.py
+++ backend-drf/api_app/views.py
@@ -2,24 +2,38 @@ from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from .serializers import PredictSerializer
-from .prediction import get_data
+
+from . import serializers, prediction
 
 
 class PredictView(CreateAPIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = PredictSerializer
+    serializer_class = serializers.PredictSerializer
     
     def create(self, request):
-        serializer = self.serializer_class(data=request.data)
+        serializer = self.serializer_class(evaluations=request.data)
         serializer.is_valid(raise_exception=True)
         
         ticker = serializer.validated_data["ticker"]
 
         try:
-            df = get_data(ticker)
+            price_data = prediction.get_stock_price_data(ticker)
         except ValueError:
             return Response({"error": "No data found for the given ticker.",
                             "status": status.HTTP_404_NOT_FOUND})
-        
-        return Response({"response": "You've passed: " + ticker, "status": "success"})
+
+        base_plot_path = prediction.plot_and_get_image_path(ticker, [price_data.Close], ["b"], ["Price"])
+        ma_plot_path = prediction.plot_and_get_image_path(ticker, [price_data.Close, price_data.ma100, price_data.ma200], ["b", "r", "g"], ["Price", "MA 100", "MA 200"])
+
+        evaluations = prediction.evaluate(price_data.Close, "api_app/stock-perdictor.keras")
+        pred_plot_path = prediction.plot_and_get_image_path(ticker, [evaluations["y_test"], evaluations["y_pred"]], ["b", "r"], ["Real Price", "Predicted Price"])
+
+        return Response({
+            "status": "success",
+            "base_plot_path": base_plot_path,
+            "ma_plot_path": ma_plot_path,
+            "pred_plot_path": pred_plot_path,
+            "mse": evaluations["mse"],
+            "rmse": evaluations["rmse"],
+            "r2_score": evaluations["r2_score"]
+        })
