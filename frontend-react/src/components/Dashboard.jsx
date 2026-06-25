2dd14a8b484c125817416987c406969458537fbe
Alireza Mohammadi <alireza1384928@gmail.com>
2026-06-25 16:43:10 +0330

    Display plots and evaluations in frontend

--- frontend-react/src/components/Dashboard.jsx
+++ frontend-react/src/components/Dashboard.jsx
@@ -3,23 +3,37 @@ import axiosInstance from "../axiosInstance"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faSpinner } from "@fortawesome/free-solid-svg-icons"
 
+const rootURL = import.meta.env.VITE_BACKEND_ROOT_URL
+
 function Dashboard() {
   const [ticker, setTicker] = useState("")
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState("")
+  const [basePlot, setBasePlot] = useState("")
+  const [maPlot, setMaPlot] = useState("")
+  const [predPlot, setPredPlot] = useState("")
+  const [evaluations, setEvaluations] = useState({})
 
   async function getPrediction(e) {
     e.preventDefault()
     setError("")
     setIsLoading(true)
+    setBasePlot("")
 
     try {
       const response = await axiosInstance.post("v1/predict/", { ticker })
-      console.log(response.data)
-      
+
       if (response.data.error) {
         setError(response.data.error)
       }
+      setBasePlot(rootURL + response.data.base_plot_path)
+      setMaPlot(rootURL + response.data.ma_plot_path)
+      setPredPlot(rootURL + response.data.pred_plot_path)
+      setEvaluations({
+        mse: response.data.mse,
+        rmse: response.data.rmse,
+        r2Score: response.data.r2_score
+      })
     } catch (err) {
       console.error("There was an error with the API request", err)
     } finally {
@@ -50,6 +64,18 @@ function Dashboard() {
             </button>
           </form>
         </div>
+        <div>
+          {basePlot && <img className="plot-image" src={basePlot} alt="plot_image" />}
+          {maPlot && <img className="plot-image" src={maPlot} alt="plot_image" />}
+          {predPlot && <img className="plot-image" src={predPlot} alt="plot_image" />}
+        </div>
+        {evaluations.mse && (
+          <div className="pt-3 text-light col-md-6 mx-auto">
+            <p>MSE: {evaluations.mse}</p>
+            <p>RMSE: {evaluations.rmse}</p>
+            <p>R<sup>2</sup> Score: {evaluations.r2Score}</p>
+          </div>
+        )}
       </div>
     </main>
   )
