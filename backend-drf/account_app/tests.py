e1b3e0af26b94c65381f80987dfd0bfe38a32a30
Alireza Mohammadi <alireza1384928@gmail.com>
2026-07-10 11:01:00 +0330

    Remove unused files

--- backend-drf/account_app/tests.py
+++
@@ -1,7 +0,0 @@
-from django.urls import reverse
-from django.contrib.auth.models import User
-from rest_framework import status
-from rest_framework.test import APITestCase
-from rest_framework.authtoken.models import Token
-
-# TODO: New testing case is needed
